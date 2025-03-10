28a7b24cf98cdd107495a5f4c425b0db134f83c8
21wsaleem <144434650+21wsaleem@users.noreply.github.com>
2025-03-10 19:34:43 +0000

    Update Program.cs
    newer

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -4,36 +4,55 @@
     {
         struct Film
         {
-            public string title;
-            public string certificate;
-            public int year;
-            public bool beingShown;
+            namespace RecordsStructureTask1
+{
+    internal class Program
+    {
+        struct Film
+        {
+       {
+        public string title;
+        public string certificate;
+        public int year;
+        public bool beingShown;
 
-            public Film(string title, string certificate, int year, bool beingShown)
+        public Film(string title, string certificate, int year, bool beingShown)
+        {
+            this.title = title;
+            this.certificate = certificate;
+            this.year = year;
+            this.beingShown = beingShown;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Film hulk = new Film("Hulk", "12A", 2005, false);
+        Film ironMan = new Film("Iron Man", "12A", 2008, false);
+        Film antMan = new Film("Ant-Man", "12A", 2015, false);
+        Film[] filmCollection = new Film[] { antMan, hulk, ironMan };
+
+
+        Film newestFilm = filmCollection[0]; 
+
+        foreach (Film film in filmCollection)
+        {
+            if (film.year > newestFilm.year)
             {
-                this.title = title;
-                this.certificate = certificate;
-                this.year = year;
-                this.beingShown = beingShown;
-
+                newestFilm = film;
             }
         }
-        static void Main(string[] args)
+
+        Console.WriteLine("The newest film is: " + newestFilm.title);
+
+       
+        foreach (Film film in filmCollection)
         {
-            Film hulk = new Film("Hulk", "12A", 2005, false);
-            Film ironMan = new Film("Iron Man","12A",2008,false);
-            Film antMan = new Film("Ant-Man", "12A", 2015, false);
-            Film[] filmCollection = new Film[] { antMan,hulk,ironMan };
-            int year = 0;
-            int position = 0;
-
-            //  Write the code to do the following
-            // Loop through the array of films and check for the newest film
-            // Produce one output to say the name of the newest film
-
-            // Write the code to update the andMan record to show the film is currently being shown
-
-
+            if (film.title == "Ant-Man")
+            {
+                film.beingShown = true;
+                Console.WriteLine(film.title + " is now being shown.");
+            }
         }
     }
 }
